cfe86333401458109435c673c7c7c117674cc091
network-Error <iamimew@gmail.com>
2024-02-19 19:38:07 +0300

    init: start task

---
+++ src/components/TreeView.jsx
@@ -0,0 +1,12 @@
+import MenuList from "./menu-list"
+import './style.css'
+
+const TreeView = ({menus = []}) => {
+  return (
+    <div className="tree-view-container">
+      <MenuList list={menus}/>
+    </div>
+  )
+}
+
+export default TreeView
